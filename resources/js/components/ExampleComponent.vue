8fc4a3fedaa203756b14e170d67cd5974671c5e3
jdiaz2108 <jdiaz2108@hotmail.com>
2019-02-15 17:49:58 -0500

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,37 @@
+<template>
+
+<v-carousel height="700px"
+            interval="8000">
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
+
+
+</template>
+
+<script>
+    import Vuetify from 'vuetify'
+    export default {
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+          }
+        ]
+      }
+    }
+  }
+</script>
